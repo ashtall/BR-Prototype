5c5f3a42939ba5e5d12eb180c61f9a00968dd022
lnx-e <lnxegg@gmail.com>
2024-05-26 20:48:15 +0530

    Randomize bullets

--- build/main.d
+++ build/main.d
@@ -23,7 +23,43 @@ main.o: D:/butano/games/BR-Prototype/src/main.cpp \
  D:/butano/butano/include/bn_optional.h \
  D:/butano/butano/include/bn_memory_alias.h \
  D:/butano/butano/include/bn_string_view.h \
- D:/butano/butano/include/bn_vblank_callback_type.h
+ D:/butano/butano/include/bn_vblank_callback_type.h \
+ D:/butano/butano/include/bn_random.h \
+ D:/butano/butano/include/bn_limits.h \
+ D:/butano/butano/include/bn_seed_random.h \
+ D:/butano/butano/include/bn_log.h \
+ D:/butano/butano/include/bn_config_log.h \
+ D:/butano/butano/include/bn_log_backend.h \
+ D:/butano/butano/include/bn_keypad.h \
+ D:/butano/butano/include/bn_sprite_text_generator.h \
+ D:/butano/butano/include/bn_vector.h \
+ D:/butano/butano/include/bn_vector_fwd.h \
+ D:/butano/butano/include/bn_fixed_point.h \
+ D:/butano/butano/include/bn_point.h \
+ D:/butano/butano/include/bn_fixed_point_fwd.h \
+ D:/butano/butano/include/bn_sprite_font.h \
+ D:/butano/butano/include/bn_sprite_item.h \
+ D:/butano/butano/include/bn_sprite_shape_size.h \
+ D:/butano/butano/include/bn_bpp_mode.h \
+ D:/butano/butano/include/bn_sprite_tiles_item.h \
+ D:/butano/butano/include/bn_tile.h \
+ D:/butano/butano/include/bn_compression_type.h \
+ D:/butano/butano/include/bn_sprite_palette_item.h \
+ D:/butano/butano/include/bn_color.h \
+ D:/butano/butano/include/bn_utf8_characters_map_ref.h \
+ D:/butano/butano/include/bn_power_of_two.h \
+ D:/butano/butano/include/bn_utf8_character.h \
+ D:/butano/butano/include/bn_string.h \
+ D:/butano/butano/include/bn_sprite_ptr.h \
+ D:/butano/butano/include/bn_time.h \
+ D:/butano/butano/include/bn_optional_fwd.h \
+ D:/butano/butano/include/bn_optional.h \
+ D:/butano/butano/include/bn_date.h \
+ D:/butano/games/BR-Prototype/include/common_variable_8x16_sprite_font.h \
+ D:/butano/butano/include/bn_sprite_font.h \
+ D:/butano/butano/include/bn_utf8_characters_map.h \
+ D:/butano/games/BR-Prototype/build/bn_sprite_items_common_variable_8x16_font.h \
+ D:/butano/butano/include/bn_sprite_item.h
 D:/butano/butano/include/bn_core.h:
 D:/butano/butano/include/bn_span.h:
 D:/butano/butano/include/bn_array.h:
@@ -51,3 +87,39 @@ D:/butano/butano/include/bn_optional.h:
 D:/butano/butano/include/bn_memory_alias.h:
 D:/butano/butano/include/bn_string_view.h:
 D:/butano/butano/include/bn_vblank_callback_type.h:
+D:/butano/butano/include/bn_random.h:
+D:/butano/butano/include/bn_limits.h:
+D:/butano/butano/include/bn_seed_random.h:
+D:/butano/butano/include/bn_log.h:
+D:/butano/butano/include/bn_config_log.h:
+D:/butano/butano/include/bn_log_backend.h:
+D:/butano/butano/include/bn_keypad.h:
+D:/butano/butano/include/bn_sprite_text_generator.h:
+D:/butano/butano/include/bn_vector.h:
+D:/butano/butano/include/bn_vector_fwd.h:
+D:/butano/butano/include/bn_fixed_point.h:
+D:/butano/butano/include/bn_point.h:
+D:/butano/butano/include/bn_fixed_point_fwd.h:
+D:/butano/butano/include/bn_sprite_font.h:
+D:/butano/butano/include/bn_sprite_item.h:
+D:/butano/butano/include/bn_sprite_shape_size.h:
+D:/butano/butano/include/bn_bpp_mode.h:
+D:/butano/butano/include/bn_sprite_tiles_item.h:
+D:/butano/butano/include/bn_tile.h:
+D:/butano/butano/include/bn_compression_type.h:
+D:/butano/butano/include/bn_sprite_palette_item.h:
+D:/butano/butano/include/bn_color.h:
+D:/butano/butano/include/bn_utf8_characters_map_ref.h:
+D:/butano/butano/include/bn_power_of_two.h:
+D:/butano/butano/include/bn_utf8_character.h:
+D:/butano/butano/include/bn_string.h:
+D:/butano/butano/include/bn_sprite_ptr.h:
+D:/butano/butano/include/bn_time.h:
+D:/butano/butano/include/bn_optional_fwd.h:
+D:/butano/butano/include/bn_optional.h:
+D:/butano/butano/include/bn_date.h:
+D:/butano/games/BR-Prototype/include/common_variable_8x16_sprite_font.h:
+D:/butano/butano/include/bn_sprite_font.h:
+D:/butano/butano/include/bn_utf8_characters_map.h:
+D:/butano/games/BR-Prototype/build/bn_sprite_items_common_variable_8x16_font.h:
+D:/butano/butano/include/bn_sprite_item.h:
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,90 @@
 #include "bn_core.h"
+#include "bn_random.h"
+#include "bn_seed_random.h"
+#include "bn_log.h"
+#include "bn_keypad.h"
+#include "bn_sprite_text_generator.h"
+#include "bn_string.h"
+#include "bn_sprite_ptr.h"
+#include "bn_time.h"
+#include "bn_optional.h"
+#include "bn_date.h"
+
+#include "common_variable_8x16_sprite_font.h"
+
+void game(bn::sprite_text_generator &text_generator)
+{
+    text_generator.set_center_alignment();
+    bn::string<24> str = "";
+
+    if (!bn::time::active())
+    {
+        BN_LOG(bn::time::active());
+        str = "RTC is not enabled";
+        bn::vector<bn::sprite_ptr, 32> text_sprites;
+        text_generator.generate(0, 0, str, text_sprites);
+        bn::core::update();
+        while (true)
+        {
+        }
+    }
+    else
+    {
+        bn::seed_random rand;
+        bn::optional<bn::time> time = bn::time::current();
+        rand.set_seed(time->hour() * 10000 + time->minute() * 100 + time->second());
+
+        int noOfBullets = 4;
+        int lowlimit = noOfBullets > 4 ? 2 : 1;
+        int uplimit = noOfBullets > 4 ? noOfBullets - 1 : noOfBullets;
+        int noOfLiveBullets = rand.get_unbiased_int(lowlimit, uplimit);
+        bool *bullets = new bool[noOfBullets];
+
+        for(int i = 0;i<noOfLiveBullets;i++){
+            bullets[i] = true;
+        }
+
+        // shuffle bullet array
+        for (int i = 0; i < noOfBullets; i++)
+        {
+            int j = rand.get_unbiased_int(3);
+            bool temp = bullets[i];
+            bullets[i] = bullets[j];
+            bullets[j] = temp;
+        }
+
+        for(int i = 0; i<noOfBullets;i++){
+            BN_LOG(bullets[i]);
+        }
+
+        BN_LOG(noOfLiveBullets, noOfBullets);
+
+        while (true)
+        {
+            if (bn::keypad::a_pressed())
+            {
+                int no = rand.get_unbiased_int(10);
+                BN_LOG("A ", no);
+                bn::string<16> diag_string = bn::to_string<16>(no);
+                str = diag_string;
+            }
+            bn::vector<bn::sprite_ptr, 32> text_sprites;
+            text_generator.generate(0, 0, str, text_sprites);
+            bn::core::update();
+        }
+    }
+}
 
 int main()
 {
     bn::core::init();
 
-    while(true)
+    bn::sprite_text_generator debug_text_gen(common::variable_8x16_sprite_font);
+
+    while (true)
     {
+
+        game(debug_text_gen);
         bn::core::update();
     }
 }
